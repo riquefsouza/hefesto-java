f64d25dacc1e3b04d0e25600f3d8eb281b9fc2bf
riquefsouza <riquefsouza@gmail.com>
2020-07-19 16:31:42 -0300

    refactoring

--- hefesto-spring/src/main/java/br/com/hfs/security/SecurityConfiguration.java
+++ hefesto-spring/src/main/java/br/com/hfs/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.hfs.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,28 +21,49 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	private HfsUserDetailsService userDetailsService;
 
 	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+	// authentication config
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService)
-		 .passwordEncoder(new BCryptPasswordEncoder());
+		.passwordEncoder(new BCryptPasswordEncoder());
 	}
-	
-	/*
-	 * public static void main(String[] args) { System.out.println(new
-	 * BCryptPasswordEncoder().encode("123456")); }
-	 */
-	
+
+	// authorization config
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/admin").hasRole("ADMIN")
-			.antMatchers("/user").hasAnyRole("ADMIN", "USER")
-			.antMatchers("/", "/h2-console").permitAll()
-			.and().formLogin();
+		http
+		.authorizeRequests()
+		.antMatchers("/css/**", "/img/**", "/js/**", "/vendor/**").permitAll()       
+		.antMatchers("/public/**").permitAll()
+		.antMatchers("/private/**").access("hasRole('USER') or hasRole('ADMIN')")
+		//.antMatchers("/anonymous*").anonymous()
+		.antMatchers("/login*").permitAll()		
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login.html")
+		//.loginProcessingUrl("/login.html")
+		.defaultSuccessUrl("/index.html", true)
+		.failureUrl("/error.html")
+		.and()
+		.logout()
+		//.deleteCookies(CookieUtil.JSESSIONID)
+		.logoutSuccessUrl("/index.html")
+        .and()
+        //.exceptionHandling().accessDeniedPage("/private/accessDenied");
+		.csrf().disable();
+		//.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
 	//static resources config(js, css, images, etc.)
 	@Override
 	public void configure(WebSecurity web) {
-		
+		web.ignoring().antMatchers("/css/**", "/img/**", "/js/**", "/vendor/**");
 	}
+
 }
